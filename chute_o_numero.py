44490244c6bc164dfdb582ff95304ac5aaf579d6
Breno Rocha <brenocaio.rocha@gmail.com>
2021-01-11 13:42:41 +0000

    Projeto 1 e 2

---
+++ chute_o_numero.py
@@ -0,0 +1,4 @@
+#Projeto chute o numero
+#Objetivo criar um algoritimo que gera um valor aleatorioe o usuario tem que chutar um numero ate acertar
+
+
--- simulador_de_dado.py
+++ simulador_de_dado.py
@@ -1,45 +1,36 @@
 # Simulador de dado
-#Simular o uso de um dado, gerando um numero de 1 até 6
-
+# Simular o uso de um dado, gerando um valor de 1 até 6
 import random
 import PySimpleGUI as sg
-
-
-
 
 class SimuladorDeDado:
     def __init__(self):
         self.valor_minimo = 1
-        self.valor_maximo = 6       
-
-        #Layout 
+        self.valor_maximo = 6
+        # Layout
         self.layout = [
-            [sg.Text('Jogar o dado')],
-            [sg.Button('sim'),sg.Button('nãos')]
+            [sg.Text('Jogar o dado?')],
+            [sg.Button('sim'),sg.Button('Não')]
         ]
-        
-    
+
     def Iniciar(self):
-        #Janela
-        self.janela = sg.Window('Simulador de dado',Layout=self.layout)
-        #Ler valores da janela
+        # criar uma janela
+        self.janela = sg.Window('Simulador de Dado',layout=self.layout)
+        # ler os valores da tela
         self.eventos, self.valores = self.janela.Read()
-        #Manipular valores
-        while True:       
-            try:
-                if self.eventos == 'sim' or self.eventos == 's':
-                    self.GerarValorDoDado()
-                elif self.eventos == 'não' or self.eventos == 'n':
-                    print('Agradecemos sua participação!')
-                else:
-                    print('Favor digitar sim ou não')
-            except:
-                print('Ocorreu um erro ao receber sua resposta')
+        # fazer alguma coisa com esses valores
+        try:
+            if self.eventos == 'sim' or self.eventos == 's':
+                self.GerarValorDoDado()
+            elif self.eventos == 'Não' or self.eventos == 'n':
+                print('Agrecemos a sua participação!')
+            else:
+                print('Favor digitar sim ou não')
+        except:
+            print('Ocorreu um erro ao receber sua resposta')
 
-
-    
     def GerarValorDoDado(self):
         print(random.randint(self.valor_minimo,self.valor_maximo))
 
 simulador = SimuladorDeDado()
-simulador.Iniciar()
+simulador.Iniciar() 
